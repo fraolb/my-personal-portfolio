6236e2239967132547f6c9983f51795d06ffb1dc
fraolb <Fraolbereket@gmail.com>
2022-08-17 16:03:38 +0300

    skills

---
+++ src/components/skills/Skills.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Skills() {
+  return (
+    <div>
+        <img src="https://ghchart.rshah.org/fraolb" alt="2016rshah's Github chart" />
+    </div>
+  )
+}
