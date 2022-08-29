680b8c13f7a718a8790acdc7e4dff599b02471e0
fraolb <Fraolbereket@gmail.com>
2022-08-29 20:24:08 +0300

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './Header.css'
+
 import CTA from './CTA'
 import IMG from "../../assets/IMG.png"
-import './Header.css'
+
 import HeaderSocials from './HeaderSocials'
 
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase-config";
+
 export default function Header() {
+  const [header, setHeader] = useState([]);
+  const headerCollection = collection(db, "header");
+
+  useEffect(() => {
+    const getFile = async () => {
+      const data = await getDocs(headerCollection);
+      setHeader(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getFile();
+  }, []);
+
   return (
       <header>
         <div className='container header__container'>
           <h5>Hello I'm</h5>
           <h1>Fraol Bereket</h1>
-          <h5 className='text-light'>Front-End Developer</h5>
+          <h5 className='text-light'>{header.map((a)=>a.title)}</h5>
           <CTA />
           <HeaderSocials />
           <div className='me'>
             <img src={IMG} alt="me" />
           </div>
           <a href='#contact' className='scroll__down'>Scroll Down</a>
-        </div>
+        </div>    
       </header> 
+      
   )  
 }
