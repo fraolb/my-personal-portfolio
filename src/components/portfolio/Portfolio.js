e7d774a63125abf4e86134094dcc80fd06abb892
fraolb <Fraolbereket@gmail.com>
2022-09-04 08:10:20 +0300

    update

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,45 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Portfolio.css";
+
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase-config";
+
 import Twitter from "../../assets/Twitter.jpg";
 
+import Bg from "../../assets/bg4.jpg";
+import { Parallax } from "react-parallax";
+
 function Portfolio() {
-  const data = [
-    {
-      id: 1,
-      image: Twitter,
-      title: "This is a Simple Twitter Clone",
-      github: "https:/github.com/fraolb",
-      demo: "https://twitter-clone-4c550.web.app/",
-    },
-    {
-      id: 2,
-      image: Twitter,
-      title: "This is a Simple Twitter Clone",
-      github: "https:/github.com/fraolb",
-      demo: "https://twitter-clone-4c550.web.app/",
-    },
-    {
-      id: 3,
-      image: Twitter,
-      title: "This is a Simple Twitter Clone",
-      github: "https:/github.com/fraolb",
-      demo: "https://twitter-clone-4c550.web.app/",
-    },
-  ];
+
+  const [comp, setComp] = useState([]);
+  const compColl = collection(db, "portfolio");
+
+  useEffect(() => {
+    const getFile = async () => {
+      const data = await getDocs(compColl);
+      setComp(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getFile();
+  }, []);
 
   return (
     <section id="portfolio">
+      <Parallax blur={2} bgImage={Bg}  strength={400}>
       <h5>My recent work</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        {data.map((arr) => {
+        {comp.map((arr) => {
           return (
             <article className="portfolio__item" key={arr.id}>
               <div className="portfolio__item-image">
-                <img src={arr.image} alt={arr.title} />
+                <img src={arr.img} alt={arr.name} height="250px"/>
               </div>
-              <h3>{arr.title} </h3>
+              <h3>{arr.name} </h3>
               <div className="portfolio__item-cta">
                 <a
                   href={arr.github}
@@ -49,7 +45,7 @@ function Portfolio() {
                   GitHub
                 </a>
                 <a
-                  href={arr.demo}
+                  href={arr.site}
                   target="_blank"
                   className="btn btn-primary"
                 >
@@ -60,6 +56,7 @@ function Portfolio() {
           );
         })}
       </div>
+      </Parallax>
     </section>
   );
 }
