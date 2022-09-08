d67674529e24360a6da96787614d2b753e366402
fraolb <Fraolbereket@gmail.com>
2022-09-08 15:47:49 +0300

    experience

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,78 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Experience.css";
+
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase-config";
+
 import { BsPatchCheckFill } from "react-icons/bs";
 
 export default function Experience() {
+  const [skill, setSkill] = useState([]);
+  const skillColl = collection(db, "skills");
+
+  useEffect(() => {
+    const getFile = async () => {
+      const data = await getDocs(skillColl);
+      setSkill(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getFile();
+  }, []);
+
   return (
     <section id="experience">
-      <h5>The skills i have</h5>
-      <h2>My Experiance</h2>
+      <h5>Languages and Frame Works I have</h5>
+      <h2>Experience On</h2>
 
       <div className="container experience__container">
         <div className="experience__frontend">
           <h3>Frontend Development</h3>
           <div className="experience__content">
-            <article className="experience__detail">
+          {skill.map((me) => {
+            if(me.type == "front_end"){
+              return <article className="experience__detail">
               <BsPatchCheckFill className="icon"/>
               <div>
-                <h4>HTML</h4>
-                <small className="text-light">Experienced</small>
+                <h4>{me.name}</h4>
+                <small className="text-light">{me.level}</small>
               </div>
             </article>
-            <article className="experience__detail">
-              <BsPatchCheckFill className="icon"/>
-              <div>
-                <h4>CSS</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__detail">
-              <BsPatchCheckFill className="icon"/>
-              <div>
-                <h4>JavaScript</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__detail">
-              <BsPatchCheckFill className="icon"/>
-              <div>
-                <h4>React</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__detail">
-              <BsPatchCheckFill className="icon"/>
-              <div>
-                <h4>BootStrap</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+            }
+              })}
           </div>
         </div>
-        <div className="experienc__backend">
+        <div className="experience__backend">
           <h3>Backend Development</h3>
           <div className="experience__content">
-            <article className="experience__detail">
+          {skill.map((me) => {
+            if(me.type == "back_end"){
+              return <article className="experience__detail">
               <BsPatchCheckFill className="icon"/>
               <div>
-                <h4>Node Js</h4>
-                <small className="text-light">Experienced</small>
+                <h4>{me.name}</h4>
+                <small className="text-light">{me.level}</small>
               </div>
             </article>
-            <article className="experience__detail">
-              <BsPatchCheckFill className="icon"/>
-              <div>
-                <h4>My SQL</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__detail">
-              <BsPatchCheckFill className="icon"/>
-              <div>
-                <h4>Phyton</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
+            }
+              })}
           </div>
         </div>
       </div>
