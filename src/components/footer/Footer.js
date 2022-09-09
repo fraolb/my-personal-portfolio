447434af35c74f4c4bf51a2d296a19f9acd1e80d
fraolb <Fraolbereket@gmail.com>
2022-09-09 07:35:33 +0300

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React,{ useState, useEffect} from 'react'
 import './Footer.css'
+
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase-config";
+
 import { AiFillFacebook } from 'react-icons/ai'
 import { FiInstagram } from 'react-icons/fi'
 import { FaTwitter } from 'react-icons/fa'
 
 export default function Footer() {
+
+  const [header, setHeader] = useState([]);
+  const headerCollection = collection(db, "header");
+
+  useEffect(() => {
+    const getFile = async () => {
+      const data = await getDocs(headerCollection);
+      setHeader(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getFile();
+  }, []);
+  const fb = header.map(a=>a.fb)
+  const ig = header.map(a=>a.ig)
+  const twitter = header.map(a=>a.twitter)
+
   return (
     <footer>
       <a href='#' className='footer__logo'>Fraol B.</a>
@@ -13,20 +32,20 @@ export default function Footer() {
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
+        <li><a href="#services">Certificates</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#testimonials">Testimonials</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className='footer__socials'>
-        <a href='https://facebook.com'><AiFillFacebook /></a>
-        <a href='https://instagram.com'><FiInstagram /></a>
-        <a href='https://twitter.com'><FaTwitter /></a>
+        <a href={fb} target="_blank"><AiFillFacebook /></a>
+        <a href={ig} target="_blank"><FiInstagram /></a>
+        <a href={twitter} target="_blank"><FaTwitter /></a>
       </div>
 
       <div className='footer__copyright'>
-        <small>&copy; Fraol tutorial. All rights reserved.</small>
+        <small>&copy; Fraol B. All rights reserved.</small>
       </div>
     </footer>
   )
