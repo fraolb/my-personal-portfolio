efcaa2762efd31d882784a3987c0d2fc0c182b41
fraolb <Fraolbereket@gmail.com>
2022-09-17 21:18:00 +0300

    cta

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import CV from '../../assets/CV.pdf'
+import React, { useState, useEffect } from 'react'
+
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase-config";
 
 function CTA() {
+  const [header, setHeader] = useState([]);
+  const headerCollection = collection(db, "header");
+
+  useEffect(() => {
+    const getFile = async () => {
+      const data = await getDocs(headerCollection);
+      setHeader(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getFile();
+  }, []);
+
+  const cv = header.map(a=>a.cv)
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={cv} download className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Contact Me</a>
     </div>
   )
