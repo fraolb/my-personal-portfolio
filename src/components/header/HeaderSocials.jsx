b23d3ad2ab2f9ef943bbde6bb40a12e0435b3a7e
fraolb <Fraolbereket@gmail.com>
2022-10-06 17:23:49 +0300

    js

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase-config";
+
 import { BsLinkedin } from 'react-icons/bs'
 import { BsGithub } from 'react-icons/bs'
 import { BsTelegram } from 'react-icons/bs'
 
 
 function HeaderSocials() {
+  const [header, setHeader] = useState([]);
+  const headerCollection = collection(db, "header");
+
+  useEffect(() => {
+    const getFile = async () => {
+      const data = await getDocs(headerCollection);
+      setHeader(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getFile();
+  }, []);
+  const git = header.map(a=>a.github)
+  const linkedin = header.map(a=>a.linkedin)
+  const tg = header.map(a=>a.telegram)
   return (
     <div className='header__socials'>
-        <a href='https://github.com/fraolb' target="_blank"><BsGithub /></a>
-        <a href='https://www.linkedin.com/in/fraol-bereket-bekele-923455235' target="_blank"><BsLinkedin /></a>
-        <a href='t.me/@chris_av' target="_blank"><BsTelegram /></a>
+        <a href={git} target="_blank"><BsGithub /></a>
+        <a href={linkedin} target="_blank"><BsLinkedin /></a>
+        <a href={tg} target="_blank"><BsTelegram /></a>
     </div>
   )
 }
